c5ec21cc8187f8c0390534efe98346ad3f5f9a89
caseyrhodes <caseywebsthedonn25@gmail.com>
2025-07-27 12:11:21 +0300

    ᴄᴀsᴇʏʀʜᴏᴅᴇs xᴍᴅ

--- commands/play.js
+++ commands/play.js
@@ -30,7 +30,7 @@ async function playCommand(sock, chatId, message) {
         const urlYt = video.url;
 
         // Fetch audio data from API
-        const response = await axios.get(`https://apis.davidcyriltech.my.id/download/ytmp4?url=${urlYt}`);
+        const response = await axios.get(`https://apis-keith.vercel.app/download/dlmp3?url=${urlYt}`);
         const data = response.data;
 
         if (!data || !data.status || !data.result || !data.result.downloadUrl) {
@@ -59,5 +59,5 @@ async function playCommand(sock, chatId, message) {
 
 module.exports = playCommand; 
 
-/*Powered by KNIGHT-BOT*
-*Credits to Keith MD*`*/
+/*Powered by Caseyrhodes*
+*Credits to api*`*/

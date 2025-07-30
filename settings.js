9dcc9e8a7599e5beb7dfa9af1508eac46b6f3d8a
telaviv21 <123queaky@gmail.com>
2025-07-30 16:26:05 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,10 +3,10 @@ const settings = {
   author: '‎',
   botName: "JINX-XMD",
   botOwner: 'Professor', // Your name
-  ownerNumber: '254112192119', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '233533416608', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
-  description: "This is a bot for managing group commands and automating tasks.",
+  description: "This is a bot for automating tasks.",
   version: "2.0.6",
 };
 
